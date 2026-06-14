3f2a94ef445a5abaab42da7f47a83c4c04dba362
ramosh99 <einfetch@gmail.com>
2026-06-14 11:29:13 +0530

    feat: add import statements for ECR repositories in staging environment

---
+++ infra/envs/staging/import-ecr.tf
@@ -0,0 +1,24 @@
+import {
+  to = module.ecr.aws_ecr_repository.this["frontend"]
+  id = "cloudmart/frontend"
+}
+
+import {
+  to = module.ecr.aws_ecr_repository.this["product-service"]
+  id = "cloudmart/product-service"
+}
+
+import {
+  to = module.ecr.aws_ecr_repository.this["order-service"]
+  id = "cloudmart/order-service"
+}
+
+import {
+  to = module.ecr.aws_ecr_repository.this["user-service"]
+  id = "cloudmart/user-service"
+}
+
+import {
+  to = module.ecr.aws_ecr_repository.this["notification-service"]
+  id = "cloudmart/notification-service"
+}
